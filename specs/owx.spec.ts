7642060ffc7db94b43d197e07271f97b6b87c411
Junyoung Choi <rokt33r.choi@gmail.com>
2019-10-01 15:27:12 +0900

    Add tests for essential methods

---
+++ specs/owx.spec.ts
@@ -0,0 +1,61 @@
+import { isValid, validate, reportValidation, owStr, owObj } from '../src'
+
+describe('isValid', () => {
+  it('returns true if input is valid', () => {
+    const input = 'Hello, World!'
+    const predicator = owStr()
+
+    const result = isValid(input, predicator)
+
+    expect(result).toBe(true)
+  })
+
+  it('returns false if input is not valid', () => {
+    const input = 'Hello, World!'
+    const predicator = owObj()
+
+    const result = isValid(input, predicator)
+
+    expect(result).toBe(false)
+  })
+})
+
+describe('validate', () => {
+  it('validates input', () => {
+    const input = 'Hello, World!'
+    const predicator = owStr()
+
+    expect(() => {
+      validate(input, predicator)
+    })
+  })
+
+  it('throws when input is invalid', () => {
+    const input = 'Hello, World!'
+    const predicator = owObj()
+
+    expect(() => {
+      validate(input, predicator)
+    }).toThrowError()
+  })
+})
+
+describe('reportValidation', () => {
+  it('returns null if input is valid', () => {
+    const input = 'Hello, World!'
+    const predicator = owStr()
+
+    const result = reportValidation(input, predicator)
+
+    expect(result).toBe(null)
+  })
+
+  it('returns error message if input is not valid', () => {
+    const input = 'Hello, World!'
+    const predicator = owObj()
+
+    const result = reportValidation(input, predicator)
+
+    expect(result).toBe('Expected value to be object, got `Hello, World!`')
+  })
+})
