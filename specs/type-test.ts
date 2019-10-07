b3174852f3334617fe525d6f1b87e277a2892c19
Junyoung Choi <rokt33r.choi@gmail.com>
2019-10-07 15:12:02 +0900

    Introduce more usage tests for owStr

--- specs/type-test.ts
+++ specs/type-test.ts
@@ -2,14 +2,41 @@ import { isValid, owShape, owStr } from '../src'
 
 function expectType<T>(value: T) {}
 
-let a: unknown
+let unknownValue: unknown
+
+// owStr#equals
+if (isValid(unknownValue, owStr().equals('a'))) {
+  expectType<'a'>(unknownValue)
+}
+
+// owStr#oneOf
+if (isValid(unknownValue, owStr().oneOf('a', 'b'))) {
+  expectType<'a' | 'b'>(unknownValue)
+}
+
+// owShape
 if (
   isValid(
-    a,
+    unknownValue,
     owShape({
       message: owStr()
     })
   )
 ) {
-  expectType<{ message: string; [key: string]: unknown }>(a)
+  expectType<{ message: string; [key: string]: unknown }>(unknownValue)
+}
+
+// owShape(exact)
+if (
+  isValid(
+    unknownValue,
+    owShape(
+      {
+        message: owStr()
+      },
+      true
+    )
+  )
+) {
+  expectType<{ message: string }>(unknownValue)
 }
