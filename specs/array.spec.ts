aac7d4d119fd76d7d73063dd7c08a5819a1914a6
Junyoung Choi <rokt33r.choi@gmail.com>
2019-10-10 01:04:44 +0900

    Add tests for length methods

--- specs/array.spec.ts
+++ specs/array.spec.ts
@@ -10,6 +10,46 @@ describe('owArr', () => {
     }).not.toThrow()
   })
 
+  describe('#empty', () => {
+    it('validate an array is empty', () => {
+      const input: any[] = []
+      const predicator = owArr().empty()
+
+      expect(() => {
+        validate(input, predicator)
+      }).not.toThrow()
+    })
+
+    it('throws when an array is not empty', () => {
+      const input: any[] = [1]
+      const predicator = owArr().empty()
+
+      expect(() => {
+        validate(input, predicator)
+      }).toThrow('Expected value to be empty, got `[1]`')
+    })
+  })
+
+  describe('#nonEpty', () => {
+    it('validate an array is not empty', () => {
+      const input: any[] = [1]
+      const predicator = owArr().nonEmpty()
+
+      expect(() => {
+        validate(input, predicator)
+      }).not.toThrow()
+    })
+
+    it('throws when an array is empty', () => {
+      const input: any[] = []
+      const predicator = owArr().nonEmpty()
+
+      expect(() => {
+        validate(input, predicator)
+      }).toThrow('Expected value to not be empty')
+    })
+  })
+
   describe('#length', () => {
     it('validates length of an array', () => {
       const input: any[] = [1]
@@ -27,6 +67,46 @@ describe('owArr', () => {
       expect(() => {
         validate(input, predicator)
       }).toThrow('Expected value to have length `1`, got `2`')
+    })
+  })
+
+  describe('#minLength', () => {
+    it('validates min length of an array', () => {
+      const input: any[] = [1, 2, 3]
+      const predicator = owArr().minLength(3)
+
+      expect(() => {
+        validate(input, predicator)
+      }).not.toThrow()
+    })
+
+    it('throws when length of an array is less than the minimum length', () => {
+      const input: any[] = [1, 2]
+      const predicator = owArr().minLength(3)
+
+      expect(() => {
+        validate(input, predicator)
+      }).toThrow('Expected value to have a minimum length `3`, got `2`')
+    })
+  })
+
+  describe('#maxLength', () => {
+    it('validates max length of an array', () => {
+      const input: any[] = [1, 2, 3]
+      const predicator = owArr().maxLength(3)
+
+      expect(() => {
+        validate(input, predicator)
+      }).not.toThrow()
+    })
+
+    it('throws when length of an array is more than the maximum length', () => {
+      const input: any[] = [1, 2, 3, 4]
+      const predicator = owArr().maxLength(3)
+
+      expect(() => {
+        validate(input, predicator)
+      }).toThrow('Expected value to have a maximum length `3`, got `4`')
     })
   })
 })
